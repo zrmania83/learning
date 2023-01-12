9df6113e4eb834a36c70a09aadd81c44a31a4e98
Alex Stanciu <tiberius.alexandru.stanciu@gmail.com>
2023-01-12 22:14:49 +0200

    exercise2

--- lesson1/cmd.js
+++ lesson1/cmd.js
@@ -1,7 +1,7 @@
 const arguments = process.argv.slice(2);
-let first_number = arguments[0].replace('--first_number=', '');
-let second_number = arguments[1].replace('--second_number=', '');
-let operation = arguments[2].replace('--operation=', '');
+let first_number = arguments.filter(element => element.includes('--first_number=')).toString().replace('--first_number=', '');
+let second_number = arguments.filter(element => element.includes('--second_number=')).toString().replace('--second_number=', '');
+let operation = arguments.filter(element => element.includes('--operation=')).toString().replace('--operation=', '');
 switch (operation) {
     case 'add':
         console.log(`Result for ${first_number} + ${second_number} = ${parseInt(first_number) + parseInt(second_number)}`);
