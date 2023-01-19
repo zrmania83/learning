bfc9a3d35e8e2cfbe453749518dbff32a26749be
zrmania83 <108953536+zrmania83@users.noreply.github.com>
2023-01-19 23:29:10 +0200

    Add files via upload

--- lesson1/command.js
+++ lesson1/command.js
@@ -1,11 +1,10 @@
 const arguments = process.argv.slice(2);
 let command = arguments.filter(element => element.includes('--command=')).toString().replace('--command=', '');
-let  ToDo= arguments.filter(element => element.includes('--ToDo=')).toString().replace('--ToDo=', '');
 const { exec } = require('child_process');
-exec(command + ToDo, (err, stdout, stderr) => {
+exec(command , (err, stdout, stderr) => {
   if (err) {
        console.error(err)
   } else {
    console.log(stdout);
    }
-});
+}); 
