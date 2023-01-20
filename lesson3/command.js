6fdb1384629c11bc0663aa1a6ca69f60d806a3d0
zrmania83 <108953536+zrmania83@users.noreply.github.com>
2023-01-20 14:34:07 +0200

    Add files via upload

--- lesson3/command.js
+++ lesson3/command.js
@@ -1,1 +1,10 @@
-
+const arguments = process.argv.slice(2);
+let command = arguments.filter(element => element.includes('--command=')).toString().replace('--command=', '');
+const { exec } = require('child_process');
+exec(command , (err, stdout, stderr) => {
+  if (err) {
+       console.error(err)
+  } else {
+   console.log(stdout);
+   }
+}); 
