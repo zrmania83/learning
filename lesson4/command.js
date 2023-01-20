e654ad497a70da7f6fe9634b00ba19b8bc7fe873
zrmania83 <108953536+zrmania83@users.noreply.github.com>
2023-01-20 21:17:31 +0200

    Add files via upload

--- lesson4/command.js
+++ lesson4/command.js
@@ -1,1 +1,17 @@
+const arguments = process.argv.slice(2);
+const fs = require('fs');
+let name = arguments.filter(element => element.includes('--name=')).toString().replace('--name=', '');
 
+for (var a = 300; a < 341; a+=2) {
+	console.log(a);
+}
+for (var b = 500; b > 395; b-=5) {
+	console.log(b);
+}
+
+// reverse 
+    const ReverseString = str => [...str].reverse().join('');
+   console.log(ReverseString(name))  
+
+const Console = new console.Console(fs.createWriteStream('work.txt'));
+Console.log((a),(b), ReverseString(name));
